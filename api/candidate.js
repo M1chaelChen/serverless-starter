c19020d7cf350076407e629e363707d49ffab780
Michael Chen <michael.chen@applydigital.co>
2018-11-14 13:47:43 -0800

    refactor to async await

--- api/candidate.js
+++ api/candidate.js
@@ -7,43 +7,40 @@ AWS.config.setPromisesDependency(require("bluebird"));
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-exports.submit = (event, context, callback) => {
-  const requestBody = JSON.parse(event.body);
-  const fullname = requestBody.fullname;
-  const email = requestBody.email;
-  const experience = requestBody.experience;
+exports.submit = async (event, context) => {
+  try {
+    const { fullname, email, experience } = JSON.parse(event.body);
 
-  if (
-    typeof fullname !== "string" ||
-    typeof email !== "string" ||
-    typeof experience !== "number"
-  ) {
-    console.error("Validation Failed");
-    callback(
-      new Error("Couldn't submit candidate because of validation errors.")
+    if (
+      typeof fullname !== "string" ||
+      typeof email !== "string" ||
+      typeof experience !== "number"
+    ) {
+      console.error("Validation Failed");
+      return new Error(
+        "Couldn't submit candidate because of validation errors."
+      );
+    }
+
+    const res = await submitCandidateP(
+      candidateInfo(fullname, email, experience)
     );
-    return;
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `Sucessfully submitted candidate with email ${email}`,
+        candidateId: res.id
+      })
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: `Unable to submit candidate with email ${email}`
+      })
+    };
   }
-
-  submitCandidateP(candidateInfo(fullname, email, experience))
-    .then(res => {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: `Sucessfully submitted candidate with email ${email}`,
-          candidateId: res.id
-        })
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      callback(null, {
-        statusCode: 500,
-        body: JSON.stringify({
-          message: `Unable to submit candidate with email ${email}`
-        })
-      });
-    });
 };
 
 const submitCandidateP = candidate => {
@@ -70,30 +67,28 @@ const candidateInfo = (fullname, email, experience) => {
   };
 };
 
-exports.list = (event, context, callback) => {
-  const params = {
-    TableName: process.env.CANDIDATE_TABLE,
-    ProjectionExpression: "id, fullname, email"
-  };
+exports.list = async (event, context) => {
+  try {
+    const params = {
+      TableName: process.env.CANDIDATE_TABLE,
+      ProjectionExpression: "id, fullname, email"
+    };
+  
+    console.log("Scanning Candidate table.");
+    const data = await dynamoDb.scan(params).promise();
 
-  console.log("Scanning Candidate table.");
-  const onScan = (err, data) => {
-    if (err) {
-      console.log(
-        "Scan failed to load data. Error JSON:",
-        JSON.stringify(err, null, 2)
-      );
-      callback(err);
-    } else {
-      console.log("Scan succeeded.");
-      return callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-          candidates: data.Items
-        })
-      });
-    }
-  };
-
-  dynamoDb.scan(params, onScan);
+    console.log("Scan succeeded.");
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        candidates: data.Items
+      })
+    };
+  } catch(err) {
+    console.log(
+      "Scan failed to load data. Error JSON:",
+      JSON.stringify(err, null, 2)
+    );
+    return err;
+  }
 };
